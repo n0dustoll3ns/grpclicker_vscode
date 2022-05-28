b1d44798adbeadea0dd63344a81e62f89e164f18
Danila Fomin <dangdancheg@gmail.com>
2022-05-28 16:11:06 +0300

    added test tree view with errors

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,11 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	
+  const rootPath =
+    vscode.workspace.workspaceFolders &&
+    vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : undefined;
 }
 
 export function deactivate() {}
