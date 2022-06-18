24de33cc95830ba3d28a71327e1cbec330c8a5fd
Danila Fomin <dangdancheg@gmail.com>
2022-06-18 22:15:24 +0300

    added wokring tree view

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
       ? vscode.workspace.workspaceFolders[0].uri.fsPath
       : undefined;
 
-  var stuff = vscode.window.registerTreeDataProvider(
+  vscode.window.registerTreeDataProvider(
     "grpc-explorer-view",
     new GrpcClickerProvider()
   );
