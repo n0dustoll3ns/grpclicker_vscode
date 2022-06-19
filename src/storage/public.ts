2bb1c0da84e403f24840883d8aff08da57f1810c
DANCHE <dangdancheg@gmail.com>
2022-06-19 12:39:41 +0300

    modified storage to be able to manipulate different storages

--- src/storage/public.ts
+++ src/storage/public.ts
@@ -1,4 +1,9 @@
+import * as vscode from "vscode";
+import { Adresses } from "./adresses";
 
 export class Storage {
-    
+  public adresses: Adresses;
+  constructor(context: vscode.ExtensionContext) {
+    this.adresses = new Adresses(context.workspaceState);
+  }
 }
