ec894aef08e7afb5cbe86ad4047a9645dfa4ce73
DANCHE <dangdancheg@gmail.com>
2022-06-19 10:37:46 +0300

    ext fix

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
       : undefined;
 
   vscode.window.registerTreeDataProvider(
-    "grpc-explorer-view",
+    "grpc-clicker",
     new GrpcClickerProvider()
   );
 }
