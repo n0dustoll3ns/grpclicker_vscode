500c95caf921778cb9cee2572a04a37b1a38a0fd
DANCHE <dangdancheg@gmail.com>
2022-06-19 11:28:51 +0300

    refactored storage method

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,22 +1,26 @@
 import { Memento } from "vscode";
 
-export enum StorageError {
+export enum ErrStorage {
   nil,
   adressExists,
 }
 
 export class Storage {
-  constructor(private storage: Memento) {}
-
-  public loadAdresses(): string[] {
-    return this.storage.get<string[]>("grpc-clicker-adresses");
+  storageAdresses: string;
+  constructor(private storage: Memento) {
+    this.storageAdresses = "grpc-clicker-adresses";
   }
 
-  public addAdress(adress: string): StorageError {
-    let adresses = this.storage.get<string[]>("grpc-clicker-adresses");
+  public loadAdresses(): string[] {
+    return this.storage.get<string[]>(this.storageAdresses);
+  }
+
+  public addAdress(adress: string): ErrStorage {
+    let adresses = this.storage.get<string[]>(this.storageAdresses);
     if (adress.includes(adress)) {
-      return StorageError.adressExists;
+      return ErrStorage.adressExists;
     }
-    
+    adresses.push(adress);
+    this.storage.update(this.storageAdresses, adresses);
   }
 }
