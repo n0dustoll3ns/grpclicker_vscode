8463b95de0ef5a880378540eb5f75aee74b4f693
DANCHE <dangdancheg@gmail.com>
2022-06-23 23:41:35 +0300

    finished grpcurl structure parsing

--- src/grpcurl/call.ts
+++ src/grpcurl/call.ts
@@ -4,9 +4,11 @@ export class Call {
   public input: Message;
   public output: Message;
   public name: string;
-  public tag: string;
   constructor(line: string) {
-    console.log(line);
-    
+    let splittedspace = line.split(" ");
+    let splittedquote = line.split(")");
+    this.name = splittedspace[3];
+    this.input = new Message(splittedquote[0]);
+    this.output = new Message(splittedquote[1]);
   }
 }
--- src/grpcurl/message.ts
+++ src/grpcurl/message.ts
@@ -1,1 +1,10 @@
-export class Message {}
+export class Message {
+  public name: string;
+  public tag: string;
+  constructor(rawmessage: string) {
+    let splitted = rawmessage.split(" ");
+    this.tag = splitted[splitted.length - 2];
+    let splittedtag = this.tag.split(".");
+    this.name = splittedtag[splittedtag.length - 1];
+  }
+}
