e377f84f8a9a521638ea0c96b73b5b0ae731316a
DANCHE <dangdancheg@gmail.com>
2022-06-25 19:26:50 +0300

    added stream check for call property

--- src/grpcurl/call.ts
+++ src/grpcurl/call.ts
@@ -2,13 +2,21 @@ import { Message } from "./message";
 
 export class Call {
   public input: Message;
+  public inputIsStream: boolean = false;
   public output: Message;
+  public outputIsStream: boolean = false;
   public name: string;
   constructor(line: string) {
     let splittedspace = line.split(" ");
-    let splittedquote = line.split(")");
     this.name = splittedspace[3];
+    let splittedquote = line.split(")");
     this.input = new Message(splittedquote[0]);
     this.output = new Message(splittedquote[1]);
+    if (splittedquote[0].includes("stream")) {
+      this.inputIsStream = true;
+    }
+    if (splittedquote[1].includes("stream")) {
+      this.outputIsStream = true;
+    }
   }
 }
