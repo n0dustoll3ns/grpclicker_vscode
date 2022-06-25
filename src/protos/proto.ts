dcf10813311e97bcd6718e0843a69416f68792d2
DANCHE <dangdancheg@gmail.com>
2022-06-25 22:21:13 +0300

    proto icon

--- src/protos/proto.ts
+++ src/protos/proto.ts
@@ -4,11 +4,10 @@ import * as path from "path";
 export class ProtoItem extends vscode.TreeItem {
   constructor(public readonly label: string) {
     super(label);
-    super.tooltip = ``;
     super.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
   }
   iconPath = {
-    light: path.join(__filename, "..", "..", "images", "light", "host.svg"),
-    dark: path.join(__filename, "..", "..", "images", "dark", "host.svg"),
+    light: path.join(__filename, "..", "..", "images", "light", "proto.svg"),
+    dark: path.join(__filename, "..", "..", "images", "dark", "proto.svg"),
   };
 }
