27bf13105c9b47f5c6717baa4b05ad8bcaf54807
Danila Fomin <dangdancheg@gmail.com>
2022-06-26 08:19:19 +0300

    added method to describe proto fields

--- src/grpcurl/field.ts
+++ src/grpcurl/field.ts
@@ -1,3 +1,3 @@
 export class Field {
-  constructor() {}
+  constructor(line: string) {}
 }
--- src/grpcurl/message.ts
+++ src/grpcurl/message.ts
@@ -4,13 +4,17 @@ import { Field } from "./field";
 export class Message {
   public name: string;
   public tag: string;
+  public fields: Field[] = [];
   constructor(rawmessage: string, public path: string) {
     let splitted = rawmessage.split(" ");
     this.tag = splitted[splitted.length - 2];
     let splittedtag = this.tag.split(".");
     this.name = splittedtag[splittedtag.length - 1];
   }
-  async fields(): Promise<Field[]> {
+  async getFields(): Promise<Field[]> {
+    if (this.fields.length > 0) {
+      return this.fields;
+    }
     const util = require("util");
     const exec = util.promisify(require("child_process").exec);
     const call = `grpcurl -import-path / -proto ${this.path} describe ${this.tag}`;
@@ -19,6 +23,12 @@ export class Message {
       vscode.window.showErrorMessage(`${stderr}`);
       return [];
     }
-    
+    let lines = `${stdout}`.split("\n");
+    lines.forEach((line) => {
+      if (line.includes(" = ") && line.includes(";")) {
+        this.fields.push(new Field(line));
+      }
+    });
+    return this.fields;
   }
 }
