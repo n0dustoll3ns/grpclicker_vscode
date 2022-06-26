e4148dffcb5b26f3cb728843bf60ff7118852e56
Danila Fomin <dangdancheg@gmail.com>
2022-06-26 07:50:32 +0300

    removed error check on service creation

--- src/grpcurl/service.ts
+++ src/grpcurl/service.ts
@@ -6,12 +6,6 @@ export class Service {
   public tag: string;
   public calls: Call[] = [];
   constructor(lines: string[], public path: string) {
-    if (lines.length < 4) {
-      vscode.window.showErrorMessage(
-        `Unable to create service from lines:\n${lines}`
-      );
-      return;
-    }
     this.tag = lines[0].split(" ")[0];
     let splittedtag = this.tag.split(".");
     this.name = splittedtag[splittedtag.length - 1];
