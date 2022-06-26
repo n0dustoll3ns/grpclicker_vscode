a98c3c9fe49d4ec0218cf05f636cdd4c08adb9a1
Danila Fomin <dangdancheg@gmail.com>
2022-06-26 08:51:05 +0300

    added map to field constructor

--- src/grpcurl/field.ts
+++ src/grpcurl/field.ts
@@ -1,3 +1,13 @@
 export class Field {
-  constructor(line: string) {}
+  public type: string;
+  public name: string;
+  constructor(line: string) {
+    let splitted = line.split(" ");
+    if (line.includes("map") && line.includes("<") && line.includes(">")) {
+      var key = line.replace("map<", "").replace(",", "");
+      var value = line.replace(">", "");
+      this.type = `map<${key}, ${value}>`;
+      this.name = splitted[4];
+    }
+  }
 }
