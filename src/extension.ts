e591e72608e55c3529dd67115601187bff5c4ad6
Danila Fomin <dangdancheg@gmail.com>
2022-06-30 20:10:27 +0300

    .

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import { ProtosTree as ProtosTreeView } from "./protos/tree";
 import * as path from "path";
 import * as fs from "fs";
 import { Storage } from "./storage/storage";
+import { getNonce } from "./nonce";
 
 export async function activate(context: vscode.ExtensionContext) {
   const grpcurl = new Grpcurl();
@@ -82,18 +83,39 @@ export async function activate(context: vscode.ExtensionContext) {
           ],
         }
       );
-      const htmlPath = vscode.Uri.file(
-        path.join(context.extensionPath, "webview", "index.html")
+      // const htmlPath = vscode.Uri.file(
+      //   path.join(context.extensionPath, "webview", "index.html")
+      // );
+      // let html = fs.readFileSync(htmlPath.fsPath, "utf8");
+      // html.replace("main.js", `${scriptUri}`);
+      // html.replace("nonce.js", `${nonce}`);
+      const nonce = getNonce();
+      const scriptUri = panel.webview.asWebviewUri(
+        vscode.Uri.joinPath(context.extensionUri, "webview", "main.js")
       );
-      const jsfile = vscode.Uri.joinPath(
-        this._extensionUri,
-        "webview",
-        "main.js"
-      );
-      const scriptUri = panel.webview.asWebviewUri(jsfile);
-      let html = fs.readFileSync(htmlPath.fsPath, "utf8");
-      html.replace("main.js", `${scriptUri}`);
-      panel.webview.html = html;
+
+      panel.webview.html = `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${panel.webview.cspSource}; img-src ${panel.webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+				<title>Cat Coding</title>
+			</head>
+			<body>
+				<h1 id="lines-of-code-counter">0</h1>
+        <button> stfu </button>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
       console.log("triggered");
     }
   );
--- webview/main.js
+++ webview/main.js
@@ -1,3 +1,12 @@
+console.log("test example after triggered");
+console.log("test example after triere");
+console.log("test example after triere");
+console.log("test example after trire");
+console.log("test example after triggd");
+console.log("test example after tggd");
+console.log("test example aftegged");
+console.log("test example afggered");
+console.log("test example ariggered");
 (function () {
   const vscode = aquireVsCodeApi();
   console.log("test example after triggered");
@@ -10,13 +19,3 @@
   console.log("test example afggered");
   console.log("test example ariggered");
 });
-
-console.log("test example after triggered");
-console.log("test example after triere");
-console.log("test example after triere");
-console.log("test example after trire");
-console.log("test example after triggd");
-console.log("test example after tggd");
-console.log("test example aftegged");
-console.log("test example afggered");
-console.log("test example ariggered");
