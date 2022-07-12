0713770bd5a26c25c7d9dcac1898f2406c55ef94
Danila Fomin <dangdancheg@gmail.com>
2022-07-12 19:49:35 +0300

    added metadata view container

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,12 +1,15 @@
 import { Memento } from "vscode";
 import { Hosts } from "./hosts";
+import { Metadata as Metas } from "./metadata";
 import { Protos as Protos } from "./protos";
 
 export class Storage {
   public hosts: Hosts;
   public protos: Protos;
+  public metas: Metas;
   constructor(memento: Memento) {
     this.hosts = new Hosts(memento);
     this.protos = new Protos(memento);
+    this.metas = new Metas(memento);
   }
 }
