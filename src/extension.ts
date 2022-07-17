05b204713c6e095f519c83a2e59da93d495020f3
DANCHE <dangdancheg@gmail.com>
2022-07-17 21:24:14 +0300

    beautified html

--- src/extension.ts
+++ src/extension.ts
@@ -128,24 +128,15 @@ export async function activate(context: vscode.ExtensionContext) {
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
-
-				<!--
-					Use a content security policy to only allow loading images from https or from our extension directory,
-					and only allow scripts that have a specific nonce.
-				-->
 				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${panel.webview.cspSource}; img-src ${panel.webview.cspSource} https:; script-src 'nonce-W3hIwRHaPGdvqvmwfzGey0vuCz2fM6Pn';">
-
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 				<link href="${stylesResetUri}" rel="stylesheet">
 				<link href="${stylesMainUri}" rel="stylesheet">
-
 				<title>Cat Coding</title>
 			</head>
 			<body>
 				<img src="https://grpc.io/img/logos/grpc-icon-color.png" width="300" />
 				<h1 id="lines-of-code-counter">0</h1>
-
 				<script nonce="W3hIwRHaPGdvqvmwfzGey0vuCz2fM6Pn" src="${scriptUri}"></script>
 			</body>
 			</html>`;
