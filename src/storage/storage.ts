2314282003cefb698172828f4ada112da5d3e8aa
Danila Fominykh <d.fominykh@ctrl2go.solutions>
2022-07-31 19:42:34 +0300

    .

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,18 +1,18 @@
 import { Memento } from "vscode";
+import { Headers } from "./headers";
 import { History } from "./history";
 import { Hosts } from "./hosts";
-import { Metadata as Metas } from "./metas";
 import { Protos as Protos } from "./protos";
 
 export class Storage {
   public hosts: Hosts;
   public protos: Protos;
-  public metas: Metas;
+  public metas: Headers;
   public history: History;
   constructor(memento: Memento) {
     this.hosts = new Hosts(memento);
     this.protos = new Protos(memento);
-    this.metas = new Metas(memento);
+    this.metas = new Headers(memento);
     this.history = new History(memento);
   }
 }
