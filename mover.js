1f33e804cf2721e9b7c3e2dcc1db5e8153a988dd
Danila Fominykh <d.fominykh@ctrl2go.solutions>
2022-08-02 23:23:45 +0300

    .

--- mover.js
+++ mover.js
@@ -8,18 +8,41 @@ if (!fs.existsSync(`dist`)) {
 const files = fs.readdirSync(`webview/dist/assets/`);
 for (const file of files) {
   if (file.endsWith(`.js`)) {
-    fs.rename("webview/dist/assets/" + file, "dist/main.js", function (err) {
-      if (err) throw err;
-      console.log("Successfully renamed - AKA moved!");
-    });
+    fs.renameSync(
+      "webview/dist/assets/" + file,
+      "dist/main.js",
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+        console.log("Successfully renamed - AKA moved!");
+      }
+    );
   }
   if (file.endsWith(`.css`)) {
-    fs.rename("webview/dist/assets/" + file, "dist/styles.css", function (err) {
-      if (err) throw err;
-      console.log("Successfully renamed - AKA moved!");
-    });
+    fs.renameSync(
+      "webview/dist/assets/" + file,
+      "dist/styles.css",
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+        console.log("Successfully renamed - AKA moved!");
+      }
+    );
   }
 }
+
+const data = fs.readFileSync("dist/main.js");
+const fd = fs.openSync("dist/main.js", "w+");
+const insert = Buffer.from("const vscode = acquireVsCodeApi();");
+fs.writeSync(fd, insert, 0, insert.length, 0);
+fs.writeSync(fd, data, 0, data.length, insert.length);
+fs.close(fd, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
 
 var copyRecursiveSync = function (src, dest) {
   var exists = fs.existsSync(src);
@@ -38,6 +61,8 @@ var copyRecursiveSync = function (src, dest) {
   }
 };
 
+fs.rmSync(`dist/tk/`, { recursive: true, force: true });
+
 try {
   copyRecursiveSync(
     `node_modules/@vscode/webview-ui-toolkit/dist/`,
@@ -46,12 +71,3 @@ try {
 } catch (e) {
   console.log(e);
 }
-
-const data = fs.readFileSync("dist/main.js");
-const fd = fs.openSync("dist/main.js", "w+");
-const insert = Buffer.from("const vscode = acquireVsCodeApi();");
-fs.writeSync(fd, insert, 0, insert.length, 0);
-fs.writeSync(fd, data, 0, data.length, insert.length);
-fs.close(fd, (err) => {
-  if (err) throw err;
-});
